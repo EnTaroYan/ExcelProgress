31cfc19d27600b1a909de318d9e21ba2d475e615
Alex Yan <1084749479@qq.com>
2018-01-31 19:48:26 +0800

    update v1.1

--- ExcelProgress/ExcelProgress.cpp
+++ ExcelProgress/ExcelProgress.cpp
@@ -93,26 +93,69 @@ int main()
 				sheet->clear(0, rowNums,0, colNums);
 
 				//保存
-				sheet->writeStr(0, 0, "商品条码");
-				sheet->writeStr(0, 1, "商品名称");
-				sheet->writeStr(0, 2, "商品单位");
-				sheet->writeStr(0, 3, "商品ID");
-				sheet->writeStr(0, 4, "库位");
-				sheet->writeStr(0, 5, "库存数量");
+				sheet->setCol(0, 0, 14.5);
+				sheet->setCol(1, 1, 33);
+				sheet->setCol(2, 2, 4);
+				sheet->setCol(3, 3, 6.5);
+				sheet->setCol(4, 4, 12.5);
+				sheet->setCol(5, 5, 6);
+				sheet->setCol(6, 6, 6);
+
+				Format* barCodeFormat = book->addFormat();
+				barCodeFormat->setAlignH(ALIGNH_LEFT);
+				barCodeFormat->setBorder(BORDERSTYLE_THIN);
+
+				Format* nameFormat = book->addFormat();
+				nameFormat->setAlignH(ALIGNH_LEFT);
+				nameFormat->setBorder(BORDERSTYLE_THIN);
+
+				Format* unitFormat = book->addFormat();
+				unitFormat->setAlignH(ALIGNH_LEFT);
+				unitFormat->setBorder(BORDERSTYLE_THIN);
+
+				Format* idFormat = book->addFormat();
+				idFormat->setAlignH(ALIGNH_LEFT);
+				idFormat->setBorder(BORDERSTYLE_THIN);
+
+				Format* posFormat = book->addFormat();
+				posFormat->setAlignH(ALIGNH_CENTER);
+				posFormat->setBorder(BORDERSTYLE_THIN);
+
+				Format* numFormat = book->addFormat();
+				numFormat->setAlignH(ALIGNH_RIGHT);
+				numFormat->setBorder(BORDERSTYLE_THIN);
+
+				Format* backupFormat = book->addFormat();
+				backupFormat->setAlignH(ALIGNH_LEFT);
+				backupFormat->setBorder(BORDERSTYLE_THIN);
+
+				sheet->writeStr(0, 0, "商品条码", barCodeFormat);
+				sheet->writeStr(0, 1, "商品名称", nameFormat);
+				sheet->writeStr(0, 2, "单位", unitFormat);
+				sheet->writeStr(0, 3, "商品ID", idFormat);
+				sheet->writeStr(0, 4, "库位", posFormat);
+				sheet->writeStr(0, 5, "库存数", numFormat);
+				sheet->writeStr(0, 6, "备注", backupFormat);
+				sheet->setRow(0, 20);
 				for (int i = 1; i < goodsList.size()+1; ++i)
 				{
-					sheet->writeStr(i, 0, (goodsList[i - 1].barcode).c_str());
-					sheet->writeStr(i, 1, (goodsList[i - 1].name).c_str());
-					sheet->writeStr(i, 2, (goodsList[i - 1].unit).c_str());
-					sheet->writeNum(i, 3, (goodsList[i - 1].id));
-					sheet->writeStr(i, 4, (goodsList[i - 1].position).c_str());
-					sheet->writeNum(i, 5, (goodsList[i - 1].num));
+					sheet->writeStr(i, 0, (goodsList[i - 1].barcode).c_str(), barCodeFormat);
+					sheet->writeStr(i, 1, (goodsList[i - 1].name).c_str(), nameFormat);
+					sheet->writeStr(i, 2, (goodsList[i - 1].unit).c_str(), unitFormat);
+					sheet->writeNum(i, 3, (goodsList[i - 1].id), idFormat);
+					sheet->writeStr(i, 4, (goodsList[i - 1].position).c_str(), posFormat);
+					sheet->writeNum(i, 5, (goodsList[i - 1].num), numFormat);
+					sheet->writeStr(i, 6, "", backupFormat);
+					sheet->setRow(i, 20);
 				}
 			}
 
+			sheet->setFooter("第&P页 共&N页",0);
+			sheet->setPaper(PAPER_A4);
+
 			if (book->save("../1.xlsx"))
 			{
-				cout << "处理完毕" << endl; cout << "处理完毕" << endl;
+				cout << "处理完毕" << endl;
 				::ShellExecute(NULL, "open", "../1.xlsx", NULL, NULL, SW_SHOW);
 			}
 			else
